3840c9f72a14a90d166230433c24b07fcd0c9515
Karoline Alencar <karolinedalencar@gmail.com>
2019-03-09 17:31:29 -0300

    Add send email

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const puppeteer = require('puppeteer');
 const fileSystem = require('fs');
-
+const nodemailer = require('nodemailer');
 
 const scrape = async () => {
     const browser = await puppeteer.launch({headless: false});
@@ -31,7 +31,75 @@ const scrape = async () => {
     });
 
     browser.close();
+    await emailSend(result);
     return result;
+}
+
+const emailSend = async (products) => {
+    const email = process.env.EMAIL;
+    const password = process.env.PASSWORD;
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: email,
+            pass: password 
+        }
+    });
+
+    const mailOptions = {
+        from: email, 
+        to: email, 
+        subject: 'The Sims 4 Prices', 
+        html: `
+            <h1>The Sims 4 Prices</h1> 
+            <ul>
+                ${products.map(product => 
+                    `<li>
+                        <h2>${product.title}</h2>
+                        <ul>
+                            ${
+                                product.price.original 
+                                ? 
+                                `<li>
+                                    <p>
+                                        <b>Original:</b> ${product.price.original}
+                                    </p>
+                                </li>`
+                                : ''
+                            }           
+                            ${
+                                product.price.discount 
+                                ? 
+                                `<li>
+                                    <p>
+                                        <b>Discount:</b> ${product.price.discount}%
+                                    </p>
+                                </li>`
+                                : ''
+                            } 
+                            ${
+                                product.price.final 
+                                ? 
+                                `<li>
+                                    <p>
+                                        <b>Final:</b> ${product.price.final}
+                                    </p>
+                                </li>`
+                                : ''
+                            } 
+                        </ul>
+                    </li>`
+                ).join('')}
+            </ul>
+        `
+    };
+    
+   await transporter.sendMail(mailOptions, function (error, info) {
+        if(error) console.log(error)
+        else console.log(info);
+   });
+
 }
 
 scrape()
